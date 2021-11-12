f5372e7ab639879cd612cbff5d2cfd21a4a04835
Kontur <sequence@myrealm.ca>
2021-11-12 16:35:35 -0700

    Implemented tasks 2,3 of module 3.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,5 @@
 function camelize(str) {
-  // ваш код...
+  let words = str.split("-");
+  words = [words[0], ...words.slice(1).map((word) => word[0].toUpperCase() + word.slice(1))];
+  return words.join("");
 }
