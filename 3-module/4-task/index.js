dac3e95f4b977262162293d0ef16aef73efdbcf7
Kontur <sequence@myrealm.ca>
2021-11-18 15:06:21 -0700

    Implemented all problems of module 4 and rewrote problem 4 of module 3.

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,9 +1,5 @@
 function showSalary(users, age) {
-  let usersSalaries = "";
-  for (const user of users) {
-    if (user.age <= age) {
-      usersSalaries += user.name + ", " + user.balance + "\n";
-    }
-  }
-  return usersSalaries.slice(0, -1);
+  let relevantUsers = users.filter((user) => user.age <= age);
+  let usersSalaries = relevantUsers.map((user) => user.name + ", " + user.balance);
+  return usersSalaries.join("\n");
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of Array.from(table.rows).slice(1)) {
+    if (tr.cells[3].getAttribute("data-available") === null) {
+      tr.setAttribute("hidden", "hidden");
+    }
+    if (tr.cells[3].getAttribute("data-available") === "true") {
+      tr.classList.add("available");
+    }
+    else {
+      tr.classList.add("unavailable");
+    }
+    if (tr.cells[2].textContent == "m") {
+      tr.classList.add("male");
+    }
+    else {
+      tr.classList.add("female");
+    }
+    if (+tr.cells[1].textContent < 18) {
+      tr.setAttribute("style", "text-decoration: line-through");
+    }
+  }
 }
