d89e77983fd7c62d2a395c4e64300bef15554513
Kontur <sequence@myrealm.ca>
2021-12-09 21:49:34 -0700

    Implemented task 2 of module 5.

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let buttons = document.getElementsByClassName('toggle-text-button');
+  let text = document.getElementById('text');
+  for (let button of buttons) {
+    button.onclick = function() {
+      text.hidden = !text.hidden;
+    };
+  }
 }
